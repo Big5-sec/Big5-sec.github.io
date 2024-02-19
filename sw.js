022cf41fa289250495a2f44287f6d35072c9d262
Nico Z <nicolas.zilio@hotmail.fr>
2024-02-19 14:54:02 +0100

    19/92/2024 more2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240219.1443';
+const cacheName = 'chirpy-20240219.1453';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
